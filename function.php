fecd6b3c30f560c31ceb5daf4c1548784f904e09
HandhikaTiaSaputro <handikatiasaputra7900@gmail.com>
2024-11-20 14:41:11 +0700

    mengupdate code sesuai dengan psr 1 dan 2

--- function.php
+++ function.php
@@ -1,27 +1,31 @@
 <?php
 require "db.php";
 
-    function tambah_data($nama, $lapangan, $tanggal, $waktu) {
+function tambah_data($nama, $lapangan, $tanggal, $waktu) 
+{
     GLOBAL $db;
 
     $tambah = $db->query("INSERT INTO bookings (nama, lapangan, tanggal, waktu) VALUES ('$nama', '$lapangan', '$tanggal', '$waktu')");
     return $tambah;
 }
 
-function ambil_data($id) {
+function ambil_data($id) 
+{
     GLOBAL $db;
     $result = $db->query("SELECT * FROM bookings WHERE id = $id");
     $booking = $result->fetchArray();
     return $booking;
 }
 
-function edit_data($nama, $lapangan, $tanggal, $waktu, $id) {
+function edit_data($nama, $lapangan, $tanggal, $waktu, $id) 
+{
     GLOBAL $db;
     $edit = $db->query("UPDATE bookings SET nama = '$nama', lapangan = '$lapangan', tanggal = '$tanggal', waktu = '$waktu' WHERE id = $id");
     return $edit;
 }
 
-function hapus($id) {
+function hapus($id) 
+{
     GLOBAL $db;
     $hapus = $db->query("DELETE FROM bookings WHERE id = $id");
     return $hapus;
